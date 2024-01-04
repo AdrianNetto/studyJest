8ac89ac943a0fadea28da87870709fa550c6a9b7
Adrian Netto <adrian.netto73@gmail.com>
2024-01-04 15:43:26 -0300

    get route and tests added

--- src/app.spec.ts
+++ src/app.spec.ts
@@ -2,11 +2,30 @@ import request from "supertest";
 import { App } from "./app";
 
 // teste suite
-describe("Test", () => {
-  // teste unitario
+describe("Tests", () => {
+  // testes unitarios
   it("should test the server running", async () => {
     const app = new App().app;
     const response = await request(app).get("/");
     expect(response.body).toStrictEqual({ ok: true });
+  });
+
+  it("should test if create user", async () => {
+    const app = new App().app;
+    const response = await request(app).post("/create-user").send({
+      nome: "Jhon",
+      email: "Jhon.test@test.com",
+      password: "123123",
+      age: 30,
+    });
+    expect(response.statusCode).toEqual(200 );
+    expect(response.body).toHaveProperty("id");
+  });
+
+  it("should test if list users", async () => {
+    const app = new App().app;
+    const response = await request(app).get("/list-users");
+    expect(response.statusCode).toEqual(200 );
+    console.log(response)
   });
 });
--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
-import express, { Application } from "express";
+import express, { Application, response } from "express";
+import crypto from "crypto";
 
 class App {
   public app: Application;
@@ -14,12 +15,35 @@ class App {
   }
 
   routes() {
+    let users: any = [];  
     this.app.get("/", (req, res) => {
       return res.json({ ok: true });
     });
+
+    this.app.post("/create-user", (req, res) => {
+      const { email, password, name, age } = req.body;
+
+      const id = crypto.randomUUID();
+
+      const data = {
+        id,
+        email,
+        password,
+        name,
+        age,
+      };
+      users.push(data);
+
+      return res.status(200).json(data);
+    });
+
+    this.app.get("/list-users", (req, res) => {
+
+      return res.status(200).json(users);
+    });
   }
 
-  listen(port: number) {
+  listen(port: number): void {
     this.app.listen(port, () => {
       console.log(`Server running on port ${port} 🏆 `);
     });
